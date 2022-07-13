42517f2b880fb3be3b3f1e25e2aa9762734f3bcb
CarlitosHunter <carlos.laurie.93@gmail.com>
2022-07-13 16:23:00 +0200

    sobre equipo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { LandingComponent } from './components/pages/landing/landing.component' ;
+import { EquipoComponent } from './components/pages/equipo/equipo.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
 
+    EquipoComponent,
     LandingComponent,
 
     FooterComponent,
