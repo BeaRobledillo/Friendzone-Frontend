9caaa795a7a03293b0555ff6c33124587491ac4d
jesusod <jesusoterodevesa@gmail.com>
2022-08-15 09:14:39 +0200

    calendar finished

--- src/app/components/pages/calendar/calendar.component.ts
+++ src/app/components/pages/calendar/calendar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/angular'; // useful for typechecking
+import { EventService } from 'src/app/services/event.service';
+import { Event } from 'src/app/interface/event';
+import esLocale from '@fullcalendar/core/locales/es';
+
 
 @Component({
   selector: 'app-calendar',
@@ -8,21 +12,63 @@ import { CalendarOptions } from '@fullcalendar/angular'; // useful for typecheck
 })
 export class CalendarComponent implements OnInit {
 
-  calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth',
-    dateClick: this.handleDateClick.bind(this), // bind is important!
-     events: [
-      { title: 'event 1', date: '2022-08-01' },
-      { title: 'event 2', date: '2022-08-03' }
-    ] 
-  };
-  handleDateClick(arg) {
-    alert('date click! ' + arg.dateStr)
-  }
+  events:Event[]=[];
 
-  constructor() { }
+  calendarOptions : CalendarOptions;
+
+  constructor(private eventService:EventService) { }
+
+  myEvents: any [] = [];
 
   ngOnInit(): void {
+
+
+    this.getEvents();
+    console.log (this.eventsParse(this.events));
+    console.log (this.calendarOptions);
+
+    this.myEvents= this.eventsParse(this.events);
+
+  }
+
+  private getEvents(){
+    this.eventService.getEventsList().subscribe(data => {
+      this.events = data;
+      console.log(data);
+      let parseEvents = this.eventsParse(data);
+      console.log(parseEvents);
+      this.calendarOptions = {
+        initialView: 'dayGridMonth',
+        locale: esLocale,
+        navLinks: false,
+        headerToolbar: {
+          left: '',
+          center: 'title',
+        },
+        
+        dateClick: this.handleDateClick.bind(this), // bind is important!
+         events: parseEvents
+      };
+
+    })
+  }
+
+  private eventsParse(events: Event[]){
+    let parseEvents: any [] = [];
+    events.forEach((item)=>{
+      let parse: any = {title:"", date:""}
+      parse.title = "";
+      parse.title = item.name;
+      parse.date = "";
+      parse.date = item.event_date;
+      parseEvents.push(parse);
+      console.log (parse);
+    });
+    return parseEvents;
+  }
+  
+  handleDateClick(arg) {
+    alert('date click! ' + arg.dateStr)
   }
 
 }
