4a80609a1d76aa4aed9111ca762128ccd92ad1bb
jesusod <jesusoterodevesa@gmail.com>
2022-08-30 11:31:23 +0200

    form fixed

--- src/app/components/layout/header-landing/header-landing.component.ts
+++ src/app/components/layout/header-landing/header-landing.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../../services/auth.service'
+import { AuthService } from '../../../services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/providers/CustomValidators';
 
 @Component({
   selector: 'app-header-landing',
@@ -8,34 +10,75 @@ import { AuthService } from '../../../services/auth.service'
   styleUrls: ['./header-landing.component.css']
 })
 export class HeaderLandingComponent implements OnInit {
-  form: any = {
-    username: null,
-    name: null,
-    email: null,
-    password: null
-  };
+
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
+
+  success = '';
+  registerForm = new FormGroup(
+    {
+      username: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ]),
+      confirmPassword: new FormControl('', [Validators.required])
+    },
+
+    CustomValidators.confirmPassword()
+  );
+
+  submitted = false;
+
   constructor(private authService: AuthService, private router:Router) { }
 
-  ngOnInit(): void {
+  get f() {
+    return this.registerForm.controls;
   }
-  onSubmit(): void {
-    const { username, name, email, password } = this.form;
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      console.log('Formulario invalido');
+      return;
+    }
+  const { username, name, email, password } = this.registerForm.value;
     this.authService.register(username, name, email, password).subscribe(
       data => {
         console.log(data);
         this.isSuccessful = true;
         this.isSignUpFailed = false;
+        
       },
       err => {
         this.errorMessage = err.error.message;
-        this.isSignUpFailed = true;
-      
-      }
-      
-    );
+        this.isSignUpFailed = true; 
+      })
+    }
+    
+
+  ngOnInit(): void {
   }
+  /* onSubmit(): void { */
+    // const { username, name, email, password } = this.form;
+    // this.authService.register(username, name, email, password).subscribe(
+    //   data => {
+    //     console.log(data);
+    //     this.isSuccessful = true;
+    //     this.isSignUpFailed = false;
+    //   },
+    //   err => {
+    //     this.errorMessage = err.error.message;
+    //     this.isSignUpFailed = true;
+      
+    //   }
+      
+    // );
+  /* } */
 
 }
